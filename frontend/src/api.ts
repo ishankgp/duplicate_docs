d03c586859d34937ab37c0dc27e91d4dbd208172
Shashank <shashank.singh@devkraft.in>
2025-10-13 14:40:53 +0530

    dev url revert

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -4,7 +4,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://172.31.9.195:8000';
+const API_BASE_URL = 'http://localhost:8000';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
